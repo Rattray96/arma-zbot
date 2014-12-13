83b9b0ab614ec65eaf84fe7a78e8503b51fe6a1b
Anthony Poschen <anthony_@live.com.au>
2014-12-14 01:01:21 +1100

    removed test code from main. that i forgot to delete. >.>.
    need to write proper tests. and test all the damn things.

--- main.go
+++ main.go
@@ -25,40 +25,4 @@ func checkError(err error) {
 func main() {
 
 	zanbot.Init()
-	//buffer := bytes.NewBuffer([]byte(""))
-
-	fileSize := int64(0)
-
-	for {
-		<-time.After(1 * time.Second)
-		fi, _ := os.Stat("./src/remoteexec.log")
-		sz := fi.Size()
-
-		var b []byte
-		switch {
-		case sz < fileSize:
-			// reread entire file should be very rare case.
-			b = make([]byte, sz)
-			fileSize = 0
-		case sz > fileSize:
-			// read from old to new
-			b = make([]byte, sz-fileSize)
-		case sz == fileSize:
-			// nothing changed who gives a shit.
-			continue
-		}
-		file, err := os.OpenFile("./src/remoteexec.log", os.O_RDONLY, 0444)
-		if err != nil && err != io.EOF {
-			panic(err)
-		}
-		file.ReadAt(b, fileSize)
-		err = file.Close()
-		if err != nil {
-			panic(err)
-		}
-		s := string(regexp.MustCompile("[^\"\n]\n\t").ReplaceAll(b, []byte("")))
-		println(s)
-		// set filesize to new save point.
-		fileSize = sz
-	}
 }
